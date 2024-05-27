0e67aa038c3c21c1c20a64542e4fe3ed8b52c98a
mmarian <mihailmarian12@gmail.com>
2024-05-27 20:56:03 +0100

    init sqlite db

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	_ "modernc.org/sqlite"
 )
 
 func main() {
@@ -34,6 +36,9 @@ func runApplication() {
 	serverRecoversFromAnyPanicAndWrites500(r)
 	allowAllOriginsForCORS(r)
 
+	initDb()
+	defer db.Close()
+
 	setUpRoutes(r)
 
 	r.Run("localhost:8080")
@@ -45,4 +50,27 @@ func serverRecoversFromAnyPanicAndWrites500(engine *gin.Engine) {
 
 func allowAllOriginsForCORS(engine *gin.Engine) {
 	engine.Use(cors.Default())
+}
+
+var db *sql.DB
+
+func initDb() {
+	var err error
+	db, err = sql.Open("sqlite", "./sqlite.db")
+	if err != nil {
+		log.Fatalf("error starting database: %s", err)
+	}
+
+	sql := `
+		create table if not exists submission (
+			user_id text primary key, 
+			content text not null, 
+			origin_url text not null
+		);
+	`
+
+	_, err = db.Exec(sql)
+	if err != nil {
+		log.Fatalf("error initializing table: %s", err)
+	}
 }
