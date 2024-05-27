2c113fce891ef437ada38e0b711b9ce9e096bf85
mmarian <mihailmarian12@gmail.com>
2024-05-27 19:57:32 +0100

    scaffolding submit

--- routes.go
+++ routes.go
@@ -8,8 +8,13 @@ import (
 
 func setUpRoutes(r *gin.Engine) {
 	r.GET("/user/:id/submissions", getSubmissions)
+	r.POST("/user/:id/submit", postSubmission)
 }
 
 func getSubmissions(c *gin.Context) {
+	c.String(http.StatusOK, "Return")
+}
+
+func postSubmission(c *gin.Context) {
 	c.String(http.StatusOK, "Return")
 }
